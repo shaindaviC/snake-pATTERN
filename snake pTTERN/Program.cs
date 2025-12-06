0baa3fc862c80069cd20d980b30ce62a2904bdcc
apap shine <shineexpos@gmail.com>
2025-12-06 12:22:49 +0530

    Add project files.

---
+++ snake pTTERN/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter matrix size n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] mat = new int[n, n];
+
+        Console.WriteLine("Enter the matrix row by row:");
+        // Example row input: 10 20 30 40
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] row = Console.ReadLine().Split(' ');
+
+            for (int j = 0; j < n; j++)
+            {
+                mat[i, j] = int.Parse(row[j]);
+            }
+        }
+
+        Console.Write("Output: ");
+
+        // Snake pattern printing
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)       // even row → left to right
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(mat[i, j] + " ");
+                }
+            }
+            else                  // odd row → right to left
+            {
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    Console.Write(mat[i, j] + " ");
+                }
+            }
+        }
+    }
+}
